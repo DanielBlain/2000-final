c0ee0e46aa2e079ecc41093f218785300fd75791
DanielBlain <dblain2@my.bcit.ca>
2023-07-24 11:32:15 -0700

    Added News Panel (empty but for title), Video Panel, js to play/pause

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,1 +1,20 @@
-// Write your JavaScript here...
+let isVideoPlaying = false;
+
+window.onload = (event) => {
+    const videoPanel = document.getElementsByClassName("video-panel")[0];
+    const videoElement = document.getElementsByTagName("video")[0];
+    const videoPanelButton = document.getElementsByClassName("play-arrow")[0];
+
+    videoPanel.onclick = (event) => {
+        if (!isVideoPlaying) {
+            videoElement.play();
+            videoPanelButton.style.display="none";
+        }
+        else {
+            videoElement.pause();
+            videoPanelButton.style.display="block";
+        }
+        isVideoPlaying = !isVideoPlaying;
+    };
+
+}
