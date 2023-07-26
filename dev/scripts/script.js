68e3aba11c4365b8953a04346a3195c62d679feb
Kaori Sato <ksato9@my.bcit.ca>
2023-07-26 06:55:55 -0700

    update shop-by and all-new

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,1 +1,10 @@
 // Write your JavaScript here...
+$(document).ready(function(){
+    $('.shop-by-slide').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+      });
+          
+  });
+      
