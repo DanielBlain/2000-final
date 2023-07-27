bd94f6190e9a371485b6a9e8f02b58734bd2e4de
Kaori Sato <ksato9@my.bcit.ca>
2023-07-27 14:53:15 -0700

    add slick

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,10 +1,38 @@
+'use strict';
 // Write your JavaScript here...
 $(document).ready(function(){
     $('.shop-by-slide').slick({
         infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3
-      });
-          
-  });
+        slidesToShow: 5,
+        slidesToScroll: 5,
+        arrows: true,
+        // dots: true,
+
+        responsive: [
+            {
+              breakpoint: 1315,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 4
+
+              }
+            },
+            {
+              breakpoint: 815,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3
+               
+              }
+            },
+            {
+              breakpoint: 450,
+              settings: "unslick"
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+         ]
+        });        
+});
       
