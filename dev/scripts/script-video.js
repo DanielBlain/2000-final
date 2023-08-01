c66df4d5723a2049d6aea6dea1e581be5397b09c
DanielBlain <dblain2@my.bcit.ca>
2023-08-01 02:10:18 -0700

    Video player finished

--- dev/scripts/script-video.js
+++ dev/scripts/script-video.js
@@ -1,9 +1,10 @@
 "use strict"
 
 function closeVideoModal() {
+    document.getElementsByTagName("video")[0].pause();
     document.getElementById("videoModal").style.display = "none";
 }
 
 function openVideoModal() {
-    document.getElementById("videoModal").style.display = "block";
+    document.getElementById("videoModal").style.display = "inline";
 }
