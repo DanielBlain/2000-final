b9c2c35a727eaa4b2694650742333ad65a26716d
DanielBlain <dblain2@my.bcit.ca>
2023-08-01 02:19:47 -0700

    Swiper unfixed, calling it a night

--- dev/scripts/script-news.js
+++ dev/scripts/script-news.js
@@ -1,39 +1,11 @@
+const swiper = document.querySelector('.news-panel').swiper;
+
 $(document).ready(function() {
-    $('.news-slick').slick({
-        respondTo: 'window',
-        responsive: [
-            {
-                breakpoint: 950,
-                settings: {
-                    slidesToShow: 5,
-                    slidesToScroll: 4,
-                }
-            },
-            {
-                breakpoint: 850,
-                settings: {
-                    slidesToShow: 4,
-                    slidesToScroll: 3,
-                }
-            },
-            {
-                breakpoint: 750,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 2,
-                }
-            },
-            {
-                breakpoint: 650,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                }
-            },
-            {
-                breakpoint: 500,
-                settings: 'unslick'
-            }
-        ],
-    })
+    const newsSwiper = new Swiper('.news-panel', {
+        speed: 400,
+        spaceBetween: 100,
+        autoplay: {
+            delay: 5000,
+        },
+    });
 })
