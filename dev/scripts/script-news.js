ed7d177f9480ce1852f084c2d796799f59d6d486
DanielBlain <dblain2@my.bcit.ca>
2023-08-04 16:54:10 -0700

    slider working but doesn't revert to block at small screen sizes

--- dev/scripts/script-news.js
+++ dev/scripts/script-news.js
@@ -1,11 +1,22 @@
-const swiper = document.querySelector('.news-panel').swiper;
+const prevArrow = document.getElementById("prevArrow");
+const nextArrow = document.getElementById("nextArrow");
+const sliderContainer = document.querySelector(".slider-container");
 
-$(document).ready(function() {
-    const newsSwiper = new Swiper('.news-panel', {
-        speed: 400,
-        spaceBetween: 100,
-        autoplay: {
-            delay: 5000,
-        },
-    });
-})
+const slideWidth = sliderContainer.clientWidth / 3;
+let currentIndex = 0;
+
+prevArrow.addEventListener("click", () => {
+    currentIndex = Math.max(currentIndex - 1, 0);
+    updateSliderPosition();
+});
+
+nextArrow.addEventListener("click", () => {
+    const maxIndex = Math.ceil(sliderContainer.scrollWidth / slideWidth) - 3;
+    currentIndex = Math.min(currentIndex + 1, maxIndex);
+    updateSliderPosition();
+});
+
+function updateSliderPosition() {
+    const translateXValue = -currentIndex * slideWidth;
+    sliderContainer.style.transform = `translateX(${translateXValue}px)`;
+}
