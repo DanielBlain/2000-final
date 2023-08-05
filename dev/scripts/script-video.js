22b03a6f4e9adf9400547854f00e42ed920d8040
Zeinab Kordeh <zeinabkordeh@gmail.com>
2023-08-05 00:29:50 -0700

    final edit

--- dev/scripts/script-video.js
+++ dev/scripts/script-video.js
@@ -10,3 +10,47 @@ function openVideoModal() {
 }
 
 
+
+
+$('.news-slick').slick({
+    respondTo: 'window',
+    nextArrow: '<i class="fa-solid fa-angle-right"></i>',
+    prevArrow: '<i class="fa-solid fa-angle-left"></i>',
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    responsive: [
+        {
+            breakpoint: 950,
+            settings: {
+                slidesToShow: 5,
+                slidesToScroll: 4,
+            }
+        },
+        {
+            breakpoint: 850,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 3,
+            }
+        },
+        {
+            breakpoint: 750,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 2,
+            }
+        },
+        {
+            breakpoint: 650,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+            }
+        },
+        {
+            breakpoint: 500,
+            settings: 'unslick'
+        }
+    ],
+})
+
